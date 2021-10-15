3a977b7d1301d3a76f261df1b9682f4627fbee57
Paul Fox <paul.fox@temposonics.com>
2021-10-15 19:15:44 -0400

    Update version for pypi wheel fix.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="netx_fdl_compiler",
-    version="0.1.1",
+    version="0.1.2",
     author="Paul Fox",
     author_email="paul.fox@temposonics.com",
     description="Flash device label compiler for Hilscher netX90",
